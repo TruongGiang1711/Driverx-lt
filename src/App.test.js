916306ad039182ce2502ae939a462c4580dff932
Giang.Lam <giang.lam@nlstech.net>
2021-08-30 19:06:33 +0700

    co cau lai structure master

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme/build'
 import App from './App'
-import ChartLineSimple from './views/charts/ChartLineSimple'
+import ChartLineSimple from './views/component/charts/ChartLineSimple'
 import Dashboard from './views/dashboard/Dashboard.js'
 
 
